ad10fbf86b441322b7ae6633f4db6b4142587676
ndido98 <ndido98@gmail.com>
2021-03-01 11:26:32 +0100

    Extract magic number

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccount.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccount.java
@@ -6,18 +6,20 @@ package lab01.example.model;
  * while the withdraw is allowed only if the balance greater or equal the withdrawal amount
  */
 public class SimpleBankAccount extends BaseBankAccount {
-    
+
+    public static final int BASE_FEE = 0;
+
     public SimpleBankAccount(final AccountHolder holder, final double balance) {
         super(holder, balance);
     }
 
     @Override
     public final void deposit(final int usrID, final double amount) {
-        deposit(usrID, amount, 0);
+        deposit(usrID, amount, BASE_FEE);
     }
 
     @Override
     public final void withdraw(final int usrID, final double amount) {
-        withdraw(usrID, amount, 0);
+        withdraw(usrID, amount, BASE_FEE);
     }
 }
